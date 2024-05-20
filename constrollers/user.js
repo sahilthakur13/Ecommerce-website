49332bbaa33e153d84463e894c4f6af69b6ccde7
sahilthakur13092003 <thakur13092003@gmail.com>
2024-05-20 10:04:54 +0530

    2nd commit

---
+++ constrollers/user.js
@@ -0,0 +1,31 @@
+const User = require('../models/user');
+const {setUser} =  require('../services/auth');
+async function handleUserSignup(req,res){
+    const {fullName, email,password,role} = req.body;
+      await User.create({
+        fullName,
+        email,
+        password,
+        role,
+    });
+    const token = setUser(User);
+    res.cookie('token',token);
+    return res.redirect('/home');
+}
+
+async function handleUserLogin(req,res){
+    const {email,password} = req.body;
+    const user = await User.findOne({email,password});
+    if(!user) 
+    return res.render('login',{
+        error:'Invalid username and password'
+    });
+    const token = setUser(user);
+    res.cookie('token',token);
+    return res.redirect('/home');
+}
+
+module.exports={
+    handleUserLogin,
+    handleUserSignup,
+}
